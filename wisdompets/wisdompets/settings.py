32515123d8c7697356d653258383a5ae07f35158
BenPrzychodzki <34103010+BenPrzychodzki@users.noreply.github.com>
2021-10-20 22:34:46 +0200

    Update settings.py

--- wisdompets/wisdompets/settings.py
+++ wisdompets/wisdompets/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9#&#ea09jorjm$+8*q^#rwl-=!g**bv1jyvkppl4#px+o3%chu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
